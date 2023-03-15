08d9d52d5e59977bc8841add7ed919504f7a12c9
Oktora Kevin Arigi <oktora.arigi@sehatq.com>
2023-03-15 23:21:15 +0700

    fix lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'src/**/*.(ts|tsx)': () => 'eslint src --ext .js,.jsx,.ts,.tsx',
+  'src/**/*.(ts|tsx)': () => 'eslint src --ext .ts,.tsx',
   'src/**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 }
